89fc9e289bd1209f61f1f5e6eb7e30003896d019
tae-lim <taelim92@gmail.com>
2018-08-15 15:41:20 -0700

    Completed app.js

--- app.js
+++ app.js
@@ -1,1 +1,52 @@
-// TODO
+class GroceryListItem extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemClick() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      textDecoration: this.state.done ? 'line-through' : 'none'
+    };
+
+    return (
+      <li style={style} onClick={this.onListItemClick.bind(this)}>{this.props.fruit}</li>
+   )
+  }
+
+  // onListItemClick(event) {
+  //   console.log('I got clicked');
+  // }
+
+};
+
+var GroceryList = (props) => (
+   <ul>
+    {props.fruits.map(fruit =>
+      <GroceryListItem fruit={fruit} />
+    )}
+   </ul>
+);
+
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList fruits={fruits}/>
+  </div>
+);
+
+var fruits = ['mango', 'strawberry']
+
+//var fruits = ['mango', 'strawberry'];
+
+
+ReactDOM.render(<App />, document.getElementById("app"))
